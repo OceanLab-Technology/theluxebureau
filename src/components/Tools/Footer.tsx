dc56234ab50689be1362d1319beb71efa3ce5b36
hanuchaudhary <kushchaudharyog@gmail.com>
2025-08-27 22:01:13 +0530

    fix: footer

--- src/components/Tools/Footer.tsx
+++ src/components/Tools/Footer.tsx
@@ -13,9 +13,10 @@ const footerData = {
   concierge: {
     title: "Concierge",
     links: [
+      { label: "Cookies", href: "/shipping" },
       { label: "Shipping & Returns", href: "/shipping" },
+      { label: "Term & Conditions", href: "/fa" },
       { label: "Contact", href: "https://www.theluxebureau.com/contact" },
-      { label: "FAQs", href: "/faqs" },
     ],
   },
   newsletter: {
@@ -45,7 +46,7 @@ const footerData = {
 
 export function Footer() {
   return (
-    <footer className="relative mt-auto min-h-[400px] mt-0 absolute top-0">
+    <footer className="relative mt-auto min-h-[300px]">
       <div className="absolute inset-0">
         <Image
           src={footerData.background.src}
@@ -58,74 +59,75 @@ export function Footer() {
       </div>
 
       <div className="relative z-10 text-[#FBF7E5]">
-        <div className="container mx-auto px-6 py-12 h-full min-h-[400px] flex flex-col justify-between">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            <div>
-              <h3 className="font-[400] font-[Marfa] text-[0.875rem] mb-4 leading-[1.625rem] tracking-[0.0875rem] uppercase">
-                {footerData.discover.title}
-              </h3>
-              <ul className="space-y-2">
-                {footerData.discover.links.map((link, index) => (
-                  <li key={index}>
-                    <Link
-                      href={link.href}
-                      className="hover:text-stone-300 transition-colors font-[Marfa]"
-                    >
-                      {link.label}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
+        <div className="mx-auto md:px-12 px-2 md:py-8 py-6 h-full min-h-[300px] flex flex-col justify-between">
+          <div className="flex md:flex-row flex-col justify-between">
+            <div className="flex md:space-x-44 gap-10">
+              <div>
+                <h3 className="font-[400] font-[Marfa] text-[0.875rem] mb-3 leading-[1.625rem] tracking-[0.0875rem] uppercase">
+                  {footerData.discover.title}
+                </h3>
+                <ul className="">
+                  {footerData.discover.links.map((link, index) => (
+                    <li key={index}>
+                      <Link
+                        href={link.href}
+                        className="hover:text-stone-300 transition-colors font-[Marfa]"
+                      >
+                        {link.label}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div>
+                <h3 className="font-[400] font-[Marfa] text-[0.875rem] mb-3 leading-[1.625rem] tracking-[0.0875rem] uppercase">
+                  {footerData.concierge.title}
+                </h3>
+                <ul className="">
+                  {footerData.concierge.links.map((link, index) => (
+                    <li key={index}>
+                      <Link
+                        href={link.href}
+                        className="hover:text-stone-300 transition-colors font-[Marfa]"
+                      >
+                        {link.label}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
 
-            <div>
-              <h3 className="font-[400] font-[Marfa] text-[0.875rem] mb-4 leading-[1.625rem] tracking-[0.0875rem] uppercase">
-                {footerData.concierge.title}
-              </h3>
-              <ul className="space-y-2 ">
-                {footerData.concierge.links.map((link, index) => (
-                  <li key={index}>
-                    <Link
-                      href={link.href}
-                      className="hover:text-stone-300 transition-colors font-[Marfa]"
-                    >
-                      {link.label}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-
-            <div>
-              <h3 className="font-[400] font-[Marfa] text-[0.875rem] mb-4 leading-[1.625rem] tracking-[0.0875rem] uppercase">
-                {footerData.newsletter.title}
-              </h3>
+            <div className="md:pt-0 pt-4">
               <div className="flex border-b justify-between">
                 <input
                   type="email"
                   placeholder={footerData.newsletter.placeholder}
-                  className="border-0 bg-transparent px-0 py-3 text-stone-200 focus:border-stone-600 text-sm focus:ring-0 outline-none rounded-none focus-visible:ring-0 placeholder:text-stone-300 font-[Marfa]"
+                  className="border-0 bg-transparent px-0 py-3 text-stone-100 focus:border-stone-600 text-[1rem] focus:ring-0 outline-none rounded-none focus-visible:ring-0 placeholder:text-stone-100 font-[Marfa]"
                 />
-                <button className="px-6 md:hidden lg:block py-2 text-white md:text-sm text-sm font-medium transition-colors font-[Marfa]">
+                <button className="md:pl-20 md:hidden lg:block py-2 text-stone-100 md:text-[1rem] font-medium transition-colors font-[Marfa]">
                   {footerData.newsletter.buttonText}
                 </button>
               </div>
             </div>
           </div>
 
-          <div className="pt-8 text-white text-sm">
+          <div className="md:pt-0 pt-4">
             <div className="md:absolute bottom-0 left-0 right-0 flex justify-center pb-10">
               <Image
                 src={footerData.logo.src}
                 alt={footerData.logo.alt}
                 width={footerData.logo.width}
                 height={footerData.logo.height}
-                className="w-35"
+                className="w-26"
                 priority={false}
               />
             </div>
-            <p className="font-[Marfa]">{footerData.copyright.text}</p>
-            <p className="font-[Marfa]">
+            <p className="font-[Marfa] md:text-[1rem] text-xs">
+              {footerData.copyright.text}
+            </p>
+            <p className="font-[Marfa] md:text-[1rem] text-xs">
               {footerData.copyright.designCredit.text}{" "}
               <Link
                 href={footerData.copyright.designCredit.href}
